b7996e311dc12f74fc6e1ddd45dd5edd4c273116
subinMl2 <skkbios@gmail.com>
2022-12-19 11:05:21 +0900

    complete RetinaNet example

--- code_example/ex_RetinaNet.py
+++ code_example/ex_RetinaNet.py
@@ -15,6 +15,8 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
 
+import random
+
 url = "https://github.com/srihari-humbarwadi/datasets/releases/download/v0.1.0/data.zip"
 filename = os.path.join(os.getcwd(), "data.zip")
 keras.utils.get_file(filename, url)
@@ -122,6 +124,7 @@ def visualize_detections(
             clip_on=True,
         )
     plt.show()
+    plt.savefig(str(int(random.random()*1000))+'_test.jpg')
     return ax
 
 ####
@@ -720,31 +723,26 @@ callbacks_list = [
 )
 
 ####
-@tf.function
-@tf.autograph.experimental.do_not_convert
-@tf.function(autograph=False)
-def test(train_dataset,val_dataset):
-    autotune = tf.data.AUTOTUNE
-    train_dataset = train_dataset.map(preprocess_data, num_parallel_calls=autotune)
-    train_dataset = train_dataset.shuffle(8 * batch_size)
-    train_dataset = train_dataset.padded_batch(
-        batch_size=batch_size, padding_values=(0.0, 1e-8, -1), drop_remainder=True
-    )
-    train_dataset = train_dataset.map(
-        label_encoder.encode_batch, num_parallel_calls=autotune
-    )
-    train_dataset = train_dataset.apply(tf.data.experimental.ignore_errors())
-    train_dataset = train_dataset.prefetch(autotune)
+autotune = tf.data.AUTOTUNE
+train_dataset = train_dataset.map(preprocess_data, num_parallel_calls=autotune)
+train_dataset = train_dataset.shuffle(8 * batch_size)
+train_dataset = train_dataset.padded_batch(
+    batch_size=batch_size, padding_values=(0.0, 1e-8, -1), drop_remainder=True
+)
+train_dataset = train_dataset.map(
+    label_encoder.encode_batch, num_parallel_calls=autotune
+)
+train_dataset = train_dataset.apply(tf.data.experimental.ignore_errors())
+train_dataset = train_dataset.prefetch(autotune)
 
-    val_dataset = val_dataset.map(preprocess_data, num_parallel_calls=autotune)
-    val_dataset = val_dataset.padded_batch(
-        batch_size=1, padding_values=(0.0, 1e-8, -1), drop_remainder=True
-    )
-    val_dataset = val_dataset.map(label_encoder.encode_batch, num_parallel_calls=autotune)
-    val_dataset = val_dataset.apply(tf.data.experimental.ignore_errors())
-    val_dataset = val_dataset.prefetch(autotune)
+val_dataset = val_dataset.map(preprocess_data, num_parallel_calls=autotune)
+val_dataset = val_dataset.padded_batch(
+    batch_size=1, padding_values=(0.0, 1e-8, -1), drop_remainder=True
+)
+val_dataset = val_dataset.map(label_encoder.encode_batch, num_parallel_calls=autotune)
+val_dataset = val_dataset.apply(tf.data.experimental.ignore_errors())
+val_dataset = val_dataset.prefetch(autotune)
 
-test(train_dataset,val_dataset)
 ####
 # Uncomment the following lines, when training on full dataset
 # train_steps_per_epoch = dataset_info.splits["train"].num_examples // batch_size
@@ -768,13 +766,39 @@ model.fit(
 )
 
 ####
+# Change this to `model_dir` when not using the downloaded weights
+weights_dir = "data"
+
+latest_checkpoint = tf.train.latest_checkpoint(weights_dir)
+model.load_weights(latest_checkpoint)
 
 
+image = tf.keras.Input(shape=[None, None, 3], name="image")
+predictions = model(image, training=False)
+detections = DecodePredictions(confidence_threshold=0.5)(image, predictions)
+inference_model = tf.keras.Model(inputs=image, outputs=detections)
+
+def prepare_image(image):
+    image, _, ratio = resize_and_pad_image(image, jitter=None)
+    image = tf.keras.applications.resnet.preprocess_input(image)
+    return tf.expand_dims(image, axis=0), ratio
 
 
+val_dataset = tfds.load("coco/2017", split="validation", data_dir="data")
+int2str = dataset_info.features["objects"]["label"].int2str
 
-
-
-if __name__ == "__main__":    
-    model_date = time.strftime("%y%m%d_%H%M%S")
+for sample in val_dataset.take(2):
+    image = tf.cast(sample["image"], dtype=tf.float32)
+    input_image, ratio = prepare_image(image)
+    detections = inference_model.predict(input_image)
+    num_detections = detections.valid_detections[0]
+    class_names = [
+        int2str(int(x)) for x in detections.nmsed_classes[0][:num_detections]
+    ]
+    visualize_detections(
+        image,
+        detections.nmsed_boxes[0][:num_detections] / ratio,
+        class_names,
+        detections.nmsed_scores[0][:num_detections],
+    )
         
